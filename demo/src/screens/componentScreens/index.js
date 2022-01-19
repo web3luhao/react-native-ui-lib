9805f17cee0d6dd33427e138078b2ea463e1e2d1
Avishai Nudelman <avishainu@gmail.com>
2022-01-19 22:23:09 +0200

    revert changes in unrelated files

--- demo/src/screens/componentScreens/index.js
+++ demo/src/screens/componentScreens/index.js
@@ -1,4 +1,6 @@
+
 export function registerScreens(registrar) {
+
   registrar('unicorn.components.ActionSheetScreen', () => require('./ActionSheetScreen').default);
   registrar('unicorn.components.ActionBarScreen', () => require('./ActionBarScreen').default);
   registrar('unicorn.components.AvatarsScreen', () => require('./AvatarsScreen').default);
@@ -26,9 +28,7 @@ export function registerScreens(registrar) {
   registrar('unicorn.components.ImageScreen', () => require('./ImageScreen').default);
   registrar('unicorn.components.GridListScreen', () => require('./GridListScreen').default);
   registrar('unicorn.components.GridViewScreen', () => require('./GridViewScreen').default);
-  registrar('unicorn.components.KeyboardAwareScrollViewScreen', () => 
-      require('./KeyboardAwareScrollViewScreen').default
-  );
+  registrar('unicorn.components.KeyboardAwareScrollViewScreen', () => require('./KeyboardAwareScrollViewScreen').default);
   registrar('unicorn.components.MaskedInputScreen', () => require('./MaskedInputScreen').default);
   registrar('unicorn.components.OverlaysScreen', () => require('./OverlaysScreen').default);
   registrar('unicorn.components.PageControlScreen', () => require('./PageControlScreen').default);
